176510ab0700a3d2bb63e1744ee1aece9e9ec3b6
홍유리 <116433637+dbfl720@users.noreply.github.com>
2022-12-11 01:43:13 +0900

    Add files via upload

---
+++ src/main/java/com/sparta/springsecurity/Scheduler/Scheduler.java
@@ -0,0 +1,4 @@
+package com.sparta.springsecurity.Scheduler;
+
+public class Scheduler {
+}
---
+++ src/main/java/com/sparta/springsecurity/security/UserDetailsImpl.java
@@ -0,0 +1,70 @@
+package com.sparta.springsecurity.security;
+
+
+import com.sparta.springsecurity.entity.User;
+import com.sparta.springsecurity.entity.UserRoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;   //인증완료된 User 객체
+    private final String username;   //
+    private final String password;
+
+    public UserDetailsImpl(User user, String username, String password) {
+        this.user = user;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);  //권한을 가지고 오는 부분
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
